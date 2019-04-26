b9e62ca7e222306162f126f728cdc44bc4c776c4
Carlil Jean <jaedagk@live.com>
2019-04-26 09:48:31 -0700

    Check.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+function addEventListenerByClass(className, event, func()){
+    var list = document.getElementsByClassName(className);
+    for(var i = 0, len = list.length; i < len; i++){
+        list[i].addEventListener(event, func, false);
+    }
+}
+$(document).getElementsByClassName('nav-link').addEventListenerByClass(this, "mouseover", function(){
+    
+})
